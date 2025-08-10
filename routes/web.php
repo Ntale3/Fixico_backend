5dc298165a1c419e0be056309c87da566fb4ac9a
Ntale Samad <swamaduntale98@gmail.com>
2025-08-10 19:17:09 +0300

    🗑 delete welcome view and route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 
-Route::get('/',function(){
-return view('welcome');
-});
 
 
 Route::get('/pay',action: [PaymentController::class,'requestToPay']);
