070330cc200ec62da5c6fc304b5cfe14c77d4fa2
Ntale Samad <swamaduntale98@gmail.com>
2025-08-15 21:50:39 +0300

    🔥 add blog routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,3 +26,20 @@ Route::prefix('auth')->group(function () {
         Route::delete('/account', [AuthController::class, 'deleteAccount']);
     });
 });
+
+
+
+
+
+Route::get('/blogs', [BlogController::class, 'index']);
+Route::get('/blogs/{blog}', [BlogController::class, 'show']);
+
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/blogs', [BlogController::class, 'store']);
+    Route::put('/blogs/{blog}', [BlogController::class, 'update']);
+    Route::post('/blogs/{blog}/comments', [BlogController::class, 'storeComment']);
+    Route::get('/my-blogs', [BlogController::class, 'myBlogs']);
+    Route::delete('/blogs/{blog}', [BlogController::class, 'destroy']);
+});
+
